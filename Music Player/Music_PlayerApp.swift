610a76ce7593bd5953280b76a29db7f4f1c19bbf
JoeShon Monroe <me@joeshonm.com>
2021-02-25 00:45:19 -0500

    Initial Commit

---
+++ Music Player/Music_PlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Music_PlayerApp.swift
+//  Music Player
+//
+//  Created by JoeShon Monroe on 2/25/21.
+//
+
+import SwiftUI
+
+@main
+struct Music_PlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
